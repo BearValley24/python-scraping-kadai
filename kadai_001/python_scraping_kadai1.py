1d9ca3f060ba1305df53f6f181ae065340e396b8
BearValley24 <150714532+BearValley24@users.noreply.github.com>
2024-02-21 01:59:28 +0900

    Change kadai_001
    課題001を修正

--- kadai_001/python_scraping_kadai1.py
+++ kadai_001/python_scraping_kadai1.py
@@ -8,20 +8,22 @@ Original file is located at
 """
 
 # 対象サイトを取得
-!pip install requests
+#!pip install requests
 
 import requests
-url = "https://news.yahoo.co.jp/articles/a12fab12de72e183c5562e11e78279a007c871e6"
+url = 'https://news.yahoo.co.jp/articles/a12fab12de72e183c5562e11e78279a007c871e6'
 response = requests.get(url)
 html = response.text
 
 # 対象サイトのHTMLを解析
-!pip install beautifulsoup4
+#!pip install beautifulsoup4
 
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(html, 'html.parser')
-p_element = soup.select_one('#uamods > div.article_body.highLightSearchTarget > div:nth-child(1) > p')
-p_text = p_element.text
 
-# 表示
-print(p_text)
+# 対象のサイトのニュース本分のp要素は同じclass名で2つある
+# findは最初に見つかった要素のみを表示、find_allは全てを表示
+# https://pystyle.info/scraping-beautiful-soup-how-to-find-elements/#outline__5_1
+p_tag = soup.find_all('p', class_='sc-lhuRmv fFWOLV yjSlinkDirectlink highLightSearchTarget')
+print(p_tag[0])
+print(p_tag[1])
